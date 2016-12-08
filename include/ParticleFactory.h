b79db1bc249e069e119242914f3b52df00676637
furinax <gpfuri@hotmail.com>
2016-12-08 00:48:03 -0500

    first commit

--- include/ParticleFactory.h
+++ include/ParticleFactory.h
@@ -4,42 +4,10 @@
 #include "ParticleSystem.h"
 #include "cinder/app/AppNative.h"
 #include "Particle_circle.h"
-#include "Particle_text.h"
-#include "Particle_halo.h"
-#include "Particle_horizon.h"
-#include "Particle_helix.h"
-#include "Particle_sphere.h"
-#include "Particle_flat.h"
-#include "Particle_field.h"
-#include "Particle_spring.h"
-#include "Particle_quad.h"
-#include "Particle_queue.h"
-#include "Particle_torrent.h"
-#include "Particle_hex.h"
-#include "Particle_trail.h"
-#include "Particle_fft.h"
-#include "Particle_ripple.h"
-#include "Particle_path.h"
 
 enum particle_choice
 {
-	PARTICLE_CIRCLE, //0
-	PARTICLE_TEXT, //1
-	PARTICLE_HALO, //2
-	PARTICLE_HORIZON, //3 red shear
-	PARTICLE_HELIX, //4 scanlines
-	PARTICLE_SPHERE, //5 polar attraction
-	PARTICLE_FLAT, //6 grid lines
-	PARTICLE_FIELD, //7 moving volume lines
-	PARTICLE_SPRING, //8 circles
-	PARTICLE_QUAD, //9 swirls
-	PARTICLE_QUEUE, //10 zooming lines
-	PARTICLE_TORRENT, //11 neural network
-	PARTICLE_HEX, //12 expanding spiral
-	PARTICLE_TRAIL, //13 simple history
-	PARTICLE_FFT, //14 fft based vis
-	PARTICLE_RIPPLE, //15 expanding circles
-	PARTICLE_PATH //16 paths leading outwards
+	PARTICLE_CIRCLE //0
 };
 
 class ParticleFactory
@@ -50,7 +18,7 @@ public:
 	//perform: for synchronization to a track
 	void perform(const std::list< ci::Vec2f > &vpos, ParticleSystem & ps);
 
-	int d_particleToCreate = 12;
+	int d_particleToCreate = 0;
 	double d_offsetTime = 0;
 	double d_adjustSeconds = 0.f; //negative means delay the visuals, positive means speed them up
 
--- src/f6_structure.cpp
+++ src/f6_structure.cpp
@@ -5,13 +5,11 @@
 #include "cinder/params/Params.h"
 #include "cinder/Camera.h"
 #include "Listener.h"
-#include "Tracker.h"
-#include "MusicPlayer.h"
 
 using namespace ci;
 using namespace ci::app;
 
-class f5_websApp : public AppNative {
+class f6_structure : public AppNative {
   public:
 	void setup();
 	void update();
@@ -27,31 +25,20 @@ class f5_websApp : public AppNative {
 	std::vector<ParticleSystem> pss;
 	bool mParticlesVisible = true;
 	ParticleFactory pf;
-	Tracker t;
 	params::InterfaceGl		mParams;
 	int mTotalParticles = 0;
-	bool mParamsVisible = true;
-	bool mTrackerVisible = false;
+	bool mParamsVisible = false;
 	bool mFullScreen = false;
 	float mVolume = 0;
-	MusicPlayer mMusicPlayer;
-	//CameraPersp mCam;
-	//Vec3f mEye, mCenter, mUp;
-	//Quatf mSceneRotation;
+
 };
 
-void f5_websApp::setup()
+void f6_structure::setup()
 {
 	// SET WINDOW
 	setWindowSize(1920, 1080);
 	setFrameRate(30.f);
 	gl::color(Color::black());
-
-	// SETUP CAMERA
-	/*mEye		= Vec3f(0.f, 0.f, -500.f);
-	mCenter		= Vec3f::zero();
-	mUp			= Vec3f::yAxis();
-	mCam.setPerspective(120.f, getWindowAspectRatio(), 5.0f, 3000.0f);*/
 
 	// SETUP PARTICLE SYSTEMS
 	ParticleSystem ps1;
@@ -63,7 +50,7 @@ void f5_websApp::setup()
 	listener.setup();
 
 	// SETUP TRACKER
-	t.setup();
+	//t.setup();
 
 	// SETUP PARAMS
 	mParams = params::InterfaceGl("Parameters", Vec2i(200, 150));
@@ -72,45 +59,33 @@ void f5_websApp::setup()
 	mParams.addParam("Total particles", &mTotalParticles, "readonly=1");
 	mParams.addParam("Volume", &mVolume, "readonly=1");
 	mParams.addParam("Scale", &listener.mScale, "min=0.1 max=40.0 step=0.1");
-	mParams.addParam("CenterX", &t.mOffset.x, "min=-200 max=200 step=5 keyIncr=D keyDecr=A");
-	mParams.addParam("CenterY", &t.mOffset.y, "min=-200 max=200 step=5 keyIncr=W keyDecr=S");
-	mParams.addParam("mScaleUpX", &t.mScaleUpAdjust.x, "min=0.00 max=2.00 step=0.05 keyIncr=r keyDecr=w");
-	mParams.addParam("mScaleUpY", &t.mScaleUpAdjust.y, "min=0.00 max=2.00 step=0.05 keyIncr=t keyDecr=g");
-	mParams.addParam("Tracker visible?", &mTrackerVisible, "");
-
-	//mParams.addParam("Scene Rotation", &mSceneRotation);
-
 	
 }
 
-void f5_websApp::mouseUp(MouseEvent event)
+void f6_structure::mouseUp(MouseEvent event)
 {
 	mMousePosition = { event.getPos(), Vec2f(getWindowWidth() - event.getPos().x, getWindowHeight() - event.getPos().y ) };
 }
-void f5_websApp::mouseDown(MouseEvent event)
+void f6_structure::mouseDown(MouseEvent event)
 {
 	mMousePosition = { event.getPos(), Vec2f(getWindowWidth() - event.getPos().x, getWindowHeight() - event.getPos().y) };
 }
-void f5_websApp::mouseDrag(MouseEvent event)
-{
-	mMousePosition = { event.getPos(), Vec2f(getWindowWidth() - event.getPos().x, getWindowHeight() - event.getPos().y) };
-}
-
-void f5_websApp::mouseMove(MouseEvent event)
+void f6_structure::mouseDrag(MouseEvent event)
 {
 	mMousePosition = { event.getPos(), Vec2f(getWindowWidth() - event.getPos().x, getWindowHeight() - event.getPos().y) };
 }
 
-void f5_websApp::keyDown(KeyEvent event)
+void f6_structure::mouseMove(MouseEvent event)
+{
+	mMousePosition = { event.getPos(), Vec2f(getWindowWidth() - event.getPos().x, getWindowHeight() - event.getPos().y) };
+}
+
+void f6_structure::keyDown(KeyEvent event)
 {
 	switch (event.getChar())
 	{
 	case 'x':
 		mParamsVisible = !mParamsVisible;
-		break;
-	case 'd': //reset to default
-		t.mOffset = Vec2f::zero();
-		t.mScaleUpAdjust = Vec2f(1.f, 1.f);
 		break;
 	case 'f':
 		mFullScreen = !mFullScreen;
@@ -119,20 +94,11 @@ void f5_websApp::keyDown(KeyEvent event)
 	}
 }
 
-void f5_websApp::update()
+void f6_structure::update()
 {
 
-	if (!mMusicPlayer.mIsStarted )
-		mMusicPlayer.start();
-
-
-	// UPDATE CAMERA
-	/*mCam.lookAt(mEye, mCenter, mUp);
-	gl::setMatrices(mCam);
-	gl::rotate(mSceneRotation);*/
-
 	// UPDATE TRACKER
-	t.update();
+	//t.update();
 
 	// UPDATE LISTENER
 	Listener& listener = Listener::getInstance();
@@ -144,29 +110,26 @@ void f5_websApp::update()
 	for (int i = 0; i < pss.size(); i++ )
 	{
 
-		pss[i].update(t.getBlobCenters());
-		pf.perform(t.getBlobCenters(), pss[i]);
+		pss[i].update(mMousePosition);
+		pf.create(mMousePosition, pss[i]);
 		particleCount += pss[i].mParticles.size();
 	}
 	mTotalParticles = particleCount;
 }
 
-void f5_websApp::draw()
+void f6_structure::draw()
 {
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) ); 
 
 	if (mParamsVisible)
 		mParams.draw();
-
-	if (mTrackerVisible)
-		t.draw();
 	
 	if (mParticlesVisible)
 	{
 		for (int i = 0; i < pss.size(); i++)
-			pss[i].draw(t.getBlobCenters());
+			pss[i].draw(mMousePosition);
 	}
 }
 
-CINDER_APP_NATIVE( f5_websApp, RendererGl )
+CINDER_APP_NATIVE( f6_structure, RendererGl )
